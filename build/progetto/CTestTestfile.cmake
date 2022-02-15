0e59646eb951bf58335e348560d5a5064ba19f27
simodiri <dirienzo.1844531@studenti.uniroma1.it>
2022-02-15 23:12:16 +0100

    first commit

---
+++ build/progetto/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/simodiri/catkin_ws/src/progetto
+# Build directory: /home/simodiri/catkin_ws/build/progetto
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
