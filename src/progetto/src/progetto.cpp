7c1e86bd6cd7df434da39c5c1c6ba273a3462984
simodiri <dirienzo.1844531@studenti.uniroma1.it>
2022-02-16 18:31:42 +0100

    first commit

--- src/progetto/src/progetto.cpp
+++ src/progetto/src/progetto.cpp
@@ -17,18 +17,8 @@ bool vel_mod=false;
 float target_x=0;
 using namespace Eigen;
 
-void VelModCallBack(const geometry_msgs::Twist::ConstPtr& msg){
- ROS_INFO("Ricevo le coordinate x:%f y:%f z:%f",msg->linear.x,msg->linear.y,msg->angular.z);
-vel_mod=true; //è arrivato un comando
- mod_vel_x=msg->linear.x;
- mod_vel_y=msg->linear.y;
- angular=msg->angular.z;
-
- target_x=mod_vel_x*0.1;
- //target_y=vel_y*0.2;
-}
-void LaserCallBack_aux(const sensor_msgs::LaserScan::ConstPtr& scan_in){
-   //devo estrarre i punti dal Laser_scan
+void LaserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_in){
+ //devo estrarre i punti dal Laser_scan
    laser_geometry::LaserProjection projector_;
    sensor_msgs::PointCloud cloud;
    tf::TransformListener listener_;
@@ -46,56 +36,21 @@ try
        Eigen::Isometry2f transform_laser = convertPose2D(transform);
   // Extract points from raw laser scan and paint them on canvas
        Eigen::Vector2f p;
-       float sum_fx=0; //somma forze su x
-       float sum_fy=0; //somma forze su y
        for(auto& point :cloud.points){
          p(0)=point.x;
          p(1)=point.y;
          
          p=transform_laser*p; //ottengo il punto trasformato
-         float modulo=1/sqrt(point.x*point.x+point.y*point.y);
-       
-         sum_fx+=p(0)*modulo*modulo; //coordinata x normalizzata
-         sum_fy+=p(1)*modulo*modulo;
        }
-       //prendiamo l'opposto delle forze applicate perchè hanno stessa direzioni ma verso opposto dei robot e ostacolo
-       sum_fx=-sum_fx;
-       sum_fy=-sum_fy;
-        geometry_msgs::Twist send;
-       float distanza=sqrt(target_x*target_x+0*0); // è la distanza dal punto in cui voglio far ruotare il robot
-     //  ROS_INFO("La distanza è %f \n", distanza);
-       
-       send.angular.z=distanza*sum_fy/30;
-       //send.angular.z=distanza;
-        //agisco sulle componenti x ed y della velocità
-        sum_fx*=abs(mod_vel_x)/600;
-        //sum_fy*=abs(mod_vel_y)/500;
-     
-     //  ROS_INFO("Le forze sono %f %f %f\n", sum_fx,sum_fy);
-       if(sum_fx<0){
-           send.linear.x= sum_fx + mod_vel_x;
-
-       }else{
-         send.linear.x=- sum_fx + mod_vel_x;
-       }
-              
-       //if(send.linear.x>0 && send.linear.x<0.1 || sum_fx>1){
-       // send.angular.z=1;
-      //}else{
-        send.angular.z+=angular;
-        // }
-       
-       send.linear.y=mod_vel_y+sum_fy;
       
-       
-       //ROS_INFO("Nuovo vettore velocità %f %f %f\n", send.linear.x,send.linear.y,send.angular.z);
+        geometry_msgs::Twist send;
+
+ 
+     
+     
+      
           
       vel_pub.publish(send);
-
-}
-void LaserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_in){
-  if(vel_mod) LaserCallBack_aux(scan_in);
-  return;
 }
 int main(int argc, char **argv){ //subscriber
    if(argc<2){
